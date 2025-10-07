3af47ddcdff7aa95816193904b6e8fe19a276731
Pumeister122 <andypu04@gmail.com>
2025-10-07 16:07:43 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,45 +1,32 @@
 import RPi.GPIO as GPIO
 import math
 import time
+
 GPIO.setmode(GPIO.BCM)
 
-list = [25, 5, 6, 13, 19, 26, 12, 16, 20, 21]    # GPIO pin number
+list = [25, 5, 6, 13, 19, 26, 12, 16, 20, 21]    # GPIO pin numbers
 list_objects = {}
-#x1 = 25
-#x2 = 5
-#x3 = 6
-#x4 = 13
-#x5 = 19
-#x6 = 26
-#x7 = 12
-#x8 = 16
-#x9 = 20
-#x10 = 21
 f = 500    # frequency (Hz)
+phase = math.pi / 9    # phase shift (radians)
 
+# set up all pins and create PWM objects
 for x in list:
-  GPIO.setup(x, GPIO.OUT)
-  list_objects[x] = GPIO.PWM(x, f) # create PWM object
-
-
-
+    GPIO.setup(x, GPIO.OUT)
+    list_objects[x] = GPIO.PWM(x, f)
+    list_objects[x].start(0)   # start at 0% duty cycle
 
 try:       # initiate PWM object
-  while True:
-    b = ((math.sin(2*math.pi*f*time.time()))**2)*100
-    list_objects[25].start(b)
-    list_objects[5].start(b+(math.pi/9))
-    list_objects[6].start(b+2*(math.pi/9))
-    list_objects[13].start(b+3*(math.pi/9))
-    list_objects[19].start(b+4*(math.pi/9))
-    list_objects[26].start(b+5*(math.pi/9))
-    list_objects[12].start(b+6*(math.pi/9)) 
-    list_objects[16].start(b+7*(math.pi/9))
-    list_objects[20].start(b+8*(math.pi/9))
-    list_objects[21].start(b+9*(math.pi/9))   
-    pass
-except KeyboardInterrupt:   # stop gracefully on ctrl-C
-  print('\nExiting')
+    while True:
+        t = time.time()
+        for i, x in enumerate(list):
+            # Apply phase shift INSIDE the sine function
+            b = (math.sin(2 * math.pi * f * t + i * phase)) ** 2 * 100
+            list_objects[x].ChangeDutyCycle(b)
 
-pwm.stop()
-GPIO.cleanup()
+except KeyboardInterrupt:   # stop gracefully on ctrl-C
+    print('\nExiting')
+
+finally:
+    for pwm in list_objects.values():
+        pwm.stop()
+    GPIO.cleanup()
