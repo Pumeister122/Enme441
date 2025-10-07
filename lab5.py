77d9cac1b8d120680649cbfcc9b0ab1ee2b468b5
Pumeister122 <andypu04@gmail.com>
2025-10-07 15:58:18 -0400

    lab5.py

--- lab5.py
+++ lab5.py
@@ -28,16 +28,16 @@ try:       # initiate PWM object
   while True:
     time = time.time()
     b = ((math.sin(2*math.pi*f*time))**2)*100
-    pwm_objects[25].start(b)
-    pwm_objects[5].start(b+(math.pi/9))
-    pwm_objects[6].start(b+2*(math.pi/9))
-    pwm_objects[13].start(b+3*(math.pi/9))
-    pwm_objects[19].start(b+4*(math.pi/9))
-    pwm_objects[26].start(b+5*(math.pi/9))
-    pwm_objects[12].start(b+6*(math.pi/9)) 
-    pwm_objects[16].start(b+7*(math.pi/9))
-    pwm_objects[20].start(b+8*(math.pi/9))
-    pwm_objects[21].start(b+9*(math.pi/9))   
+    list_objects[25].start(b)
+    list_objects[5].start(b+(math.pi/9))
+    list_objects[6].start(b+2*(math.pi/9))
+    list_objects[13].start(b+3*(math.pi/9))
+    list_objects[19].start(b+4*(math.pi/9))
+    list_objects[26].start(b+5*(math.pi/9))
+    list_objects[12].start(b+6*(math.pi/9)) 
+    list_objects[16].start(b+7*(math.pi/9))
+    list_objects[20].start(b+8*(math.pi/9))
+    list_objects[21].start(b+9*(math.pi/9))   
     pass
 except KeyboardInterrupt:   # stop gracefully on ctrl-C
   print('\nExiting')
