50e92327dd7cadcb6dc9923471edab607e1944bb
Pumeister122 <andypu04@gmail.com>
2025-10-07 16:46:14 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -4,16 +4,25 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
+button = 4
+
 list = [25, 5, 6, 13, 19, 26, 12, 16, 20, 21]    # GPIO pin numbers
 list_objects = {}
 f = 0.2    # frequency (Hz)
-phase = math.pi / 9    # phase shift (radians)
-
+phase = math.pi / 9 * direction
+direction = 1
 # set up all pins and create PWM objects
 for x in list:
     GPIO.setup(x, GPIO.OUT)
     list_objects[x] = GPIO.PWM(x, 500)
     list_objects[x].start(0)   # start at 0% duty cycle
+
+def myCallback(pin):
+    global direction
+    direction *= -1
+
+GPIO.add_event_detect(button,gpio.RISING,callback=myCallback,bouncetime = 100)
+
 
 try:       # initiate PWM object
     while True:
