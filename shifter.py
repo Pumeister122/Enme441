a401a33af835380b869f5ebb885c95a6a6a1e114
Pumeister122 <andypu04@gmail.com>
2025-10-19 15:22:25 -0400

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -1,23 +1,28 @@
-import RPi.GPIO as GPIO
 import time
-
-GPIO.setmode(GPIO.BCM)
+import RPi.GPIO as GPIO
 
 class Shifter:
-    def __init__(self, dataPin, latchPin, clockPin):
-        self.dataPin = dataPin
-        self.latchPin = latchPin
-        self.clockPin = clockPin
-        GPIO.setup(self.dataPin, GPIO.OUT)
-        GPIO.setup(self.latchPin, GPIO.OUT, initial=0)
-        GPIO.setup(self.clockPin, GPIO.OUT, initial=0)
-    def _ping(self, pin):
-        GPIO.output(pin, 1)
+    def __init__(self, data_pin, latch_pin, clock_pin):
+        self.data_pin = data_pin
+        self.latch_pin = latch_pin
+        self.clock_pin = clock_pin
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.data_pin, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.latch_pin, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.clock_pin, GPIO.OUT, initial=GPIO.LOW)
+
+    def _pulse(self, pin):
+        GPIO.output(pin, GPIO.HIGH)
         time.sleep(0.00001)
-        GPIO.output(pin, 0)
+        GPIO.output(pin, GPIO.LOW)
         time.sleep(0.00001)
-    def shiftByte(self, pattern):
-        for i in range(8):
-            GPIO.output(self.dataPin, (pattern >> i) & 1)
-            self._ping(self.clockPin)
-        self._ping(self.latchPin)
+
+    def shiftByte(self, value):
+        for bit in range(8):
+            GPIO.output(self.data_pin, (value >> bit) & 1)
+            self._pulse(self.clock_pin)
+        self._pulse(self.latch_pin)
+
+    def clear(self):
+        self.shiftByte(0)
